e1755c92bb98675a9f3776bef1c0e1d568abdea0
Nave Cohen <nave@monday.com>
2026-09-15 01:45:13 +0300

    stack-parent: nave/setup/openshell-v0.0.116-compat

--- crates/openshell-server/migrations/sqlite/004_add_object_metadata.sql
+++
@@ -1,5 +0,0 @@
--- Add labels column to support Kubernetes-style object metadata
-ALTER TABLE objects ADD COLUMN labels TEXT;
-
--- Backfill existing rows with empty labels
-UPDATE objects SET labels = '{}' WHERE labels IS NULL;
